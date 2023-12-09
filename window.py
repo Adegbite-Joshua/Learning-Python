96a9781ba8af19a967085a4f8ddd778a60aafcd2
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-12-09 11:33:11 +0100

    Working on creating a window application

---
+++ window.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk() # Instantiate an instance of a window
+window.geometry('500x500')
+window.title('PROADE CODE')
+
+# icon = PhotoImage(file='logo.png')
+# window.iconphoto(True, icon)
+window.config(background='black')
+
+window.mainloop() # Place window on a computer screen, listen for event
