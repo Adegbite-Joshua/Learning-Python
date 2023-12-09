7ae209e4fc24e835254d9515d8832adb26f708bf
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2023-12-09 01:08:19 +0100

    Worked on formatting text

--- start.py
+++ start.py
@@ -1,7 +1,22 @@
 numbers = [1,2,3,4,5,6,7,8,9]
 
+details = [
+    {
+        "name": "Adegbite Joshua",
+        "stack": "Full Stack"  
+    },
+    {
+        "name": "Kolins John",
+        "stack": "Front End"
+    }
+    ]
+
 # print_number = lambda x: print(x)
 
 # map(print_number, numbers)
 for i in numbers:
     print(i, end=' ')
+print()
+
+for detail in details:
+    print("{} using {} stack".format(detail["name"], detail["stack"]))
