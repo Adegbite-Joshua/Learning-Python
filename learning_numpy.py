02b710f6b283dd2bdeed840ffd2cc0c3180f3ea9
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2024-03-19 23:10:20 +0100

    Learnt how to create numpy arrays with built in functions

--- learning_numpy.py
+++ learning_numpy.py
@@ -48,9 +48,52 @@ empty = np.empty((4,4,4))
 # print(empty)
 
 x_values = np.arange(0,100,4)
-print(x_values)
+# print(x_values)
 
 x_values = np.linspace(0,100,5)
 x_values2 = np.linspace(0,1000,5)
-print(x_values)
-print(x_values2)
+# print(x_values)
+# print(x_values2)
+
+l1 = [1,2,3,4,5]
+l2 = [6,7,8,9,0]
+
+a1 = np.array([1,2,3,4,5])
+a2 = np.array([6,7,8,9,0])
+
+# print(l1 + l2)
+# print(a1 + a2)
+# print(a1 * a2)
+# print(a1 % a2)
+# print(a1 - a2)
+
+# print(np.append(a1, ['a','b', 'c']))
+# a1 = np.append(a1, ['a','b', 'c'])
+# print(a1)
+
+a = np.array([[1,2,3,4,5],
+              [6,7,8,9,10],
+              [11,12,13,14,15],
+              [16,17,18,19,20]])
+
+# print(a.shape)
+# # print(a.reshape((5,2,1,1,2)))
+# a.reshape((5,2,1,1,2))
+# print(a)
+
+# a.resize((5,2,1,1,2))
+# print(a)
+
+# print(a.flatten())
+
+# var1 = a.ravel()
+# var1[1] = 100
+
+# print(var1)
+# print(a)
+
+# number = np.random.randint(95,100)
+# numbers = np.random.randint(95,100, size=(2,3,4))
+
+# print(number)
+
