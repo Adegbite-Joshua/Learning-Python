4b9e4af6890c16395bd654924f407c8f31cc0d4a
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2024-03-22 13:46:46 +0100

    Working on large data set

--- learning_pandas.py
+++ learning_pandas.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 # Loading data
 
@@ -46,7 +47,49 @@ df = df[cols[0:4] + [cols[-1]] + cols[4: -2]]
 
 # print(df.head(5))
 # df.to_csv('modified.csv', index=False)
-df.to_excel('modified.xlsx', index=False)
+# df.to_excel('modified.xlsx', index=False)
 # df.to_csv('modified.txt', index=False, sep='\t')
 
-  
+
+
+# Filtering data
+
+# new_df = df.loc[(df['Type 1'] == 'Grass') &  (df['Type 2'] ==  'Poison') & (df['HP'] > 70)]
+# new_df = new_df.reset_index(drop=True)
+# new_df.reset_index(drop=True, inplace=True)
+# print(new_df)
+# new_df.to_csv('filtered.csv', index=False)
+# print(df.loc[df['Type 1'].str.contains('Fire|Grass', flags=re.I,regex=True)])
+# print(df.loc[df['Name'].str.contains('^pi[a-z]*', flags=re.I,regex=True)])
+
+
+
+
+# Conditional Changes
+# df.loc[df['Type 1'] == 'Fire', 'Type 1'] = 'Flamer'
+# df.loc[df['Total'] > 500, ['Generation', 'Legendary']] = ['Value 1', 'Value 2']
+# print(df)
+
+
+
+# Aggregate Statistics (groupby)
+# df['Defense'] = pd.to_numeric(df['Defense'], errors='coerce')
+# print(df.groupby(['Type 1']).sum().sort_values('Defense', ascending=False))
+# print(type(df['Defense']))
+
+# df['count'] = 1
+# print(df.groupby(['Type 1', 'Type 2']).count()['count'])
+
+
+
+
+# Working with large data
+new_df = pd.DataFrame(columns=df.columns)
+for df in pd.read_csv('modified.csv', chunksize=5):
+    # print('CHUNK DF')
+    # print(df)
+    results = df.groupby(['Type 1']).count()
+    # print(results)
+    new_df = pd.concat([new_df, results])
+    
+print(new_df.head(10))
