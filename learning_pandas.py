dec3b88248da9bf7ec39535847ccf5f841f66b99
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2024-03-22 11:32:11 +0100

    Worked on sorting amd making changes to the data

--- learning_pandas.py
+++ learning_pandas.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+# Loading data
+
 df = pd.read_csv('pokemon_data.csv')
 # df_xlsx = pd.read_excel('pokemon_data.xlsx')
 # df_txt = pd.read_csv('pokemon_data.csv', delimiter='\t')
@@ -9,4 +11,34 @@ df = pd.read_csv('pokemon_data.csv')
 # df_xlsx
 
 
+# Reading data from the dataframe
+
 # print(df.head(5))
+# print(df.columns)
+# print(df[['Name', 'Type 1', 'HP']][-10:])
+# print(df.iloc[0:10][['Name', 'HP']])
+# print(df.iloc[[2, 1], [5, 4]])
+# for index, row in df.iterrows():
+#  if index <= 10:
+#    print(index, row[['Name', 'HP']])
+# print(df.loc[df['Type 1'] == 'Grass' ][0:10])
+
+
+
+# Sorting and describing data
+
+# print(df.describe())
+# print(df.sort_values(['Name', 'Type 1'], ascending=[1,0]))
+
+
+
+# Making changes to the data
+
+# print(df.head(5))
+# df['Total'] = df['HP'] + df['Attack'] + df['Defense'] + df['Sp. Atk'] + df['Sp. Def'] + df['Speed']
+# print(df.head(5))
+# df = df.drop(columns=['Total'])
+df['Total'] = df.iloc[:,4:10].sum(axis=1)
+
+print(df)
+  
