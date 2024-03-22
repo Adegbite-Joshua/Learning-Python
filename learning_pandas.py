42cd813dc99ba153df0f289d59e6e1a014d44b62
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2024-03-22 01:10:14 +0100

    Learning python pandas

---
+++ learning_pandas.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv('pokemon_data.csv')
+# df_xlsx = pd.read_excel('pokemon_data.xlsx')
+# df_txt = pd.read_csv('pokemon_data.csv', delimiter='\t')
+
+# df
+# df_txt
+# df_xlsx
+
+
+# print(df.head(5))
