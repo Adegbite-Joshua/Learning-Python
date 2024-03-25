f9e85d4f98422b29d6dd95f0dbb33e62f7a3234d
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2024-03-25 01:37:41 +0100

    Modifying and updata data

--- practicing_pandas.py
+++ practicing_pandas.py
@@ -8,8 +8,9 @@ df = pd.read_csv('pokemon_data.csv')
 df['Total'] = df.iloc[:,4:9].sum(axis=1)
 df['Average'] = df.iloc[:,4:9].mean(axis=1)
 # df['Average'] = df.iloc[:,4:9]
-df.loc[( df['Type 2'].isna()), 'Type 2'] = 'NULL'
-print(df.iloc[4,3])
+df.loc[( df['Type 2'].isnull()), 'Type 2'] = 'NULL'
+df.loc[ df['Type 1'].str.lower() == 'bug', 'Legendary'] = True
+# print(df.iloc[4,3])
 # print(df.iloc[4,3].dtype)
 # print(df.dtypes)
-print(df.head(5))
+print(df.head(15))
